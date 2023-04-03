e8cc1ebdf8c29ccee4818bf1c560dfbd9c884868
trieu <longgiang9999.gl@gmail.com>
2023-04-03 23:35:46 +0700

    trien khai cac phuong thuc cua arraylist

---
+++ src/MyList.java
@@ -0,0 +1,60 @@
+
+
+public class MyList<E> {
+    private int size = 0;
+    private static int default_capacity = 10;
+    private Object[] element;
+
+    public MyList() {
+    }
+
+    public MyList(int capacity) {
+        this.default_capacity = capacity;
+    }
+
+    public void add(int index, E element) {
+
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public void size() {
+        return;
+    }
+
+    public E clone() {
+
+        return null;
+    }
+
+    public boolean contains(E o) {
+
+        return false;
+    }
+
+    public int indexOf(E o) {
+
+        return 0;
+    }
+
+    public boolean add(E e) {
+
+        return false;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+
+    }
+
+    public E get(int i) {
+
+        return null;
+    }
+        public void clear(){
+
+        }
+
+
+}
