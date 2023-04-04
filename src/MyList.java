bbedb23e009d6787481a302a2ce68b74fb512dd6
trieu <longgiang9999.gl@gmail.com>
2023-04-04 14:00:32 +0700

    trien khai cac phuong thuc cua arraylist

--- src/MyList.java
+++ src/MyList.java
@@ -1,60 +1,39 @@
-
+import java.util.Arrays;
+import java.util.PrimitiveIterator;
 
 public class MyList<E> {
     private int size = 0;
-    private static int default_capacity = 10;
+    private static int DEFAUL_CAPACITY = 10;
     private Object[] element;
 
     public MyList() {
+        element = new Object[DEFAUL_CAPACITY];
     }
-
-    public MyList(int capacity) {
-        this.default_capacity = capacity;
+//     Cài đặt phương thức ensureCapa()
+//
+//Mục đích: Tăng gấp đôi kích thước mảng chứa các phần tử
+    private void ensureCapa(){
+        int newSize = element.length*2;
+        element = Arrays.copyOf(element, newSize);
     }
-
-    public void add(int index, E element) {
-
-    }
-
-    public E remove(int index) {
-        return null;
-    }
-
-    public void size() {
-        return;
-    }
-
-    public E clone() {
-
-        return null;
-    }
-
-    public boolean contains(E o) {
-
-        return false;
-    }
-
-    public int indexOf(E o) {
-
-        return 0;
-    }
-
-    public boolean add(E e) {
-
-        return false;
-    }
-
-    public void ensureCapacity(int minCapacity) {
-
-    }
-
-    public E get(int i) {
-
-        return null;
-    }
-        public void clear(){
-
+    // cài đặt phương thức add để thêm 1 phần tử vào cuối của danh sách
+    //có tham so đầu vào là phần tử cần thêm vào danh sách.
+    public void add(E e){
+        if(size == element.length){
+            ensureCapa();
         }
+        element[size++] = e;
+    }
+//     Cài đặt phương thức get()
+//Mục đích: Phương thức get() trả về phần tử vừa ở vị trí thứ i trong danh sách.
+//Tham số đầu vào: số nguyên chứa vị trí cần truy cập đến phần tử trong danh sách
+    public E get(int i){
+        if ( i >= size || i < 0){
+            throw  new IndexOutOfBoundsException("index: "+ i + ", size" + i);
+        }
+        return (E) element[i];
+    }
+
 
 
 }
--- src/MyListTest.java
+++ src/MyListTest.java
@@ -1,5 +1,17 @@
 public class MyListTest {
     public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println("phantu thu 4:" + listInteger.get(4));
+        System.out.println("phantu thu 2:" + listInteger.get(2));
+        System.out.println("phantu thu 1:" + listInteger.get(1));
+       listInteger.get(-1);
+        System.out.println("phantu thu -1:" + listInteger.get(-1));
 
     }
 }
